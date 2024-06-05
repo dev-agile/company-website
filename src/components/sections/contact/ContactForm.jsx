7d10859025bb98282a5412eaaf3fb19dea803d61
Saurabh Singh <saurabh.singhdev15@gmail.com>
2024-06-05 10:19:39 +0530

    Added Slack notification

--- src/components/sections/contact/ContactForm.jsx
+++ src/components/sections/contact/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "tailwindcss/tailwind.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
@@ -12,16 +12,40 @@ const validationSchema = Yup.object().shape({
     .matches(/^[0-9]+$/, "Phone number is not valid")
     .required("Phone number is required"),
   message: Yup.string().required("Message is required"),
-  file: Yup.mixed().required("File is required"), // Ensure you handle file uploads correctly on the server-side
+  file: Yup.mixed().required("File is required"),
   estimatedBudget: Yup.number().required("Estimated budget is required"),
   priority: Yup.string().required("Priority is required"),
   country: Yup.string().required("Country is required"),
 });
 
 const ContactForm = () => {
-  const [fieldValue, setFieldValue] = useState();
-  const handleSubmit = (values) => {
-    console.log("Form data", values);
+  const handleSubmit = async (values, { resetForm }) => {
+    console.log("Form data", values.firstName);
+
+    const slackWebhookUrl = "https://hooks.slack.com/services/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";
+    const message = {
+      text: `New contact form submission from ${values.firstName} ${values.lastName}\nEmail: ${values.email}`,
+    };
+
+    try {
+      const response = await fetch(slackWebhookUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+
+      alert("Form submitted successfully!");
+      resetForm();
+    } catch (error) {
+      console.error("Error sending Slack notification:", error);
+      alert("There was an error submitting the form. Please try again.");
+    }
   };
 
   return (
@@ -31,7 +55,6 @@ const ContactForm = () => {
           <div className="flex justify-center items-center flex-col ">
             <div className="border rounded-full sm:p-5 p-2 shadow-lg bg-gradient-to-r from-[#83aaff] to-[#254975]">
               <img
-                const
                 src="https://samartheme2.vercel.app/images/team/large/pic3.jpg"
                 alt="Profile"
                 className="rounded-full w-72 h-72"
@@ -65,7 +88,7 @@ const ContactForm = () => {
               projectTitle: "",
               service: "",
               message: "",
-              file: undefined,
+              file: null,
               estimatedBudget: "",
               priority: "",
               country: "",
@@ -73,188 +96,181 @@ const ContactForm = () => {
             validationSchema={validationSchema}
             onSubmit={handleSubmit}
           >
-            <Form className="sm:px-8 px-1 pt-6 pb-8 mb-4">
-              <p className="text-3xl font-bold dark:text-white mb-6">
-                Contact form
-              </p>
-              <div className="mb-4 flex justify-between gap-2 sm:gap-7">
-                <Field
-                  name="firstName"
-                  type="text"
-                  placeholder="First Name"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <Field
-                  name="lastName"
-                  type="text"
-                  placeholder="Last Name"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </div>
-              <div className="mb-4 flex justify-between">
-                <ErrorMessage
-                  name="firstName"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-                <ErrorMessage
-                  name="lastName"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-4  flex justify-between gap-3 sm:gap-7">
-                <Field
-                  name="email"
-                  type="email"
-                  placeholder="Email Address"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <Field
-                  name="phone"
-                  type="text"
-                  placeholder="Phone No."
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </div>
+            {({ setFieldValue }) => (
+              <Form className="sm:px-8 px-1 pt-6 pb-8 mb-4">
+                <p className="text-3xl font-bold dark:text-white mb-6">
+                  Contact form
+                </p>
+                <div className="mb-4 flex justify-between gap-2 sm:gap-7">
+                  <Field
+                    name="firstName"
+                    type="text"
+                    placeholder="First Name"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <Field
+                    name="lastName"
+                    type="text"
+                    placeholder="Last Name"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-4 flex justify-between">
+                  <ErrorMessage
+                    name="firstName"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                  <ErrorMessage
+                    name="lastName"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
+                <div className="mb-4  flex justify-between gap-3 sm:gap-7">
+                  <Field
+                    name="email"
+                    type="email"
+                    placeholder="Email Address"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <Field
+                    name="phone"
+                    type="text"
+                    placeholder="Phone No."
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
 
-              <div className="mb-4 flex justify-between gap-3 sm:gap-7">
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-                <ErrorMessage
-                  name="phone"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4 flex justify-between gap-3 sm:gap-7">
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                  <ErrorMessage
+                    name="phone"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              <div className="mb-4 flex justify-between gap-2 sm:gap-7">
-                <Field
-                  name="projectTitle"
-                  type="text"
-                  placeholder="Project Title"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <Field
-                  name="service"
-                  as="select"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                >
-                  <option value="">Choose Service</option>
-                  <option value="service1">Service 1</option>
-                  <option value="service2">Service 2</option>
-                  <option value="service3">Service 3</option>
-                </Field>
-              </div>
+                <div className="mb-4 flex justify-between gap-2 sm:gap-7">
+                  <Field
+                    name="projectTitle"
+                    type="text"
+                    placeholder="Project Title"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <Field
+                    name="service"
+                    as="select"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  >
+                    <option value="">Choose Service</option>
+                    <option value="service1">Service 1</option>
+                    <option value="service2">Service 2</option>
+                    <option value="service3">Service 3</option>
+                  </Field>
+                </div>
 
-              <div className="mb-4 flex justify-between gap-2 sm:gap-7">
-                <ErrorMessage
-                  name="projectTitle"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-                <ErrorMessage
-                  name="service"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4 flex justify-between gap-2 sm:gap-7">
+                  <ErrorMessage
+                    name="projectTitle"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                  <ErrorMessage
+                    name="service"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              <div className="mb-4">
-                <Field
-                  as="textarea"
-                  name="message"
-                  placeholder="Message"
-                  className="shadow appearance-none border rounded-lg h-40 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="message"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4">
+                  <Field
+                    as="textarea"
+                    name="message"
+                    placeholder="Message"
+                    className="shadow appearance-none border rounded-lg h-40 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage
+                    name="message"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              <div className="mb-4">
-                <input
-                  id="file"
-                  name="file"
-                  type="file"
-                  onChange={(event) => {
-                    setFieldValue("file", event.currentTarget.files[0]);
-                  }}
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="file"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              <div className="mb-4">
-                <Field
-                  name="estimatedBudget"
-                  type="text"
-                  placeholder="Estimated Budget"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="estimatedBudget"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4">
+                  <input
+                    id="file"
+                    name="file"
+                    type="file"
+                    onChange={(event) => {
+                      setFieldValue("file", event.currentTarget.files[0]);
+                    }}
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage
+                    name="file"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
+                <div className="mb-4">
+                  <Field
+                    name="estimatedBudget"
+                    type="text"
+                    placeholder="Estimated Budget"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage
+                    name="estimatedBudget"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              {/* Priority select field */}
-              <div className="mb-4">
-                <Field
-                  as="select"
-                  name="priority"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                >
-                  <option value="">Priority</option>
-                  <option value="high">High</option>
-                  <option value="medium">Medium</option>
-                  <option value="low">Low</option>
-                </Field>
-                <ErrorMessage
-                  name="priority"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4">
+                  <Field
+                    as="select"
+                    name="priority"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  >
+                    <option value="">Priority</option>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+                  </Field>
+                  <ErrorMessage
+                    name="priority"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              <div className="mb-4">
-                <Field
-                  name="country"
-                  type="text"
-                  placeholder="Country"
-                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <ErrorMessage
-                  name="country"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
+                <div className="mb-4">
+                  <Field
+                    name="country"
+                    type="text"
+                    placeholder="Country"
+                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <ErrorMessage
+                    name="country"
+                    component="div"
+                    className="text-red-500 text-xs italic"
+                  />
+                </div>
 
-              {/* <div className="flex items-center justify-between">
-                <button
-                  type="submit"
-                  className="bg-gradient-to-r from-[#83AAFF] to-[#254975] p-1  text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                <Button
+                  borderRadius="10px"
+                  className="bg-transparent  text-blue-400 font-bold text-lg border-[1px] border-blue-400   dark:border-slate-800"
                 >
                   Submit Now
-                </button>
-              </div> */}
-              <Button
-                borderRadius="10px"
-                className="bg-transparent  text-blue-400 font-bold text-lg border-[1px] border-blue-400   dark:border-slate-800"
-              >
-               Submit Now
-              </Button>
-            </Form>
+                </Button>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
