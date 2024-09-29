af00d00b949dfb97652ecd047e8305fff5ec8aed
Saurabh Singh <saurabh@Saurabhs-MacBook-Pro.local>
2024-09-29 19:19:17 +0530

    name change

--- src/components/sections/Testimonial.jsx
+++ src/components/sections/Testimonial.jsx
@@ -4,7 +4,7 @@ import TestimonialCard from "../cards/TestimonialCard";
 const testimonials = [
   {
     description:
-      "We chose AgileGeeks to build our financial literacy and money management app from start to finish. From the first call, we were very impressed with Appinventiv’s professionalism, expertise, and commitment to delivering top-notch results. Our app is now live and the feedback that we are receiving from users is fantastic.",
+      "We chose Cognivac to build our financial literacy and money management app from start to finish. From the first call, we were very impressed with Appinventiv’s professionalism, expertise, and commitment to delivering top-notch results. Our app is now live and the feedback that we are receiving from users is fantastic.",
     name: "Cak Dikin",
     role: "Co-Founder & CEO ",
     company: "American",
@@ -15,7 +15,7 @@ const testimonials = [
   },
   {
     description:
-      "We approached AgileGeeks with a clear vision to build a robust and future-ready platform that could seamlessly integrate with the busy lifestyle of our customers while uplifting their overall experience and giving us a competitive edge. The AgileGeeks team not only understood our vision but also enhanced the overall solution with their expertise. The end results exceeded our expectations with massive improvements in the user base and digital revenue for different brands.",
+      "We approached Cognivac with a clear vision to build a robust and future-ready platform that could seamlessly integrate with the busy lifestyle of our customers while uplifting their overall experience and giving us a competitive edge. The Cognivac team not only understood our vision but also enhanced the overall solution with their expertise. The end results exceeded our expectations with massive improvements in the user base and digital revenue for different brands.",
     name: "Billy Lan",
     role: "Director - Digital Engineering",
     company: "Caley Peace",
@@ -25,7 +25,7 @@ const testimonials = [
   },
   {
     description:
-      "We were looking for an agency that would understand the direness of the hourly worker job search cycle situation when we came across AgileGeeks. What we liked about the team is how they did not just understand what we were looking for but also gave us ideas on how we could make the process more efficient and simplified for the jobseekers through their empathy mapping skillset.",
+      "We were looking for an agency that would understand the direness of the hourly worker job search cycle situation when we came across Cognivac. What we liked about the team is how they did not just understand what we were looking for but also gave us ideas on how we could make the process more efficient and simplified for the jobseekers through their empathy mapping skillset.",
     name: "Neeraj Tiwari",
     role: "CTO and Co-founder,",
     company: "Fox Dexon",
